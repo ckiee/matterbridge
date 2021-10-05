4059f16f04f9627eaa9e50c3307c2d5f4314dd42
ckie <git-525ff67@ckie.dev>
2021-10-05 09:57:51 +0300

    MOREEE

--- bridge/discord/discord.go
+++ bridge/discord/discord.go
@@ -67,7 +67,7 @@ func (b *Bdiscord) Connect() error {
 	b.c.AddHandler(b.memberRemove)
 	// Add privileged intent for guild member tracking. This is needed to track nicks
 	// for display names and @mention translation
-	b.c.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAllWithoutPrivileged | discordgo.IntentsGuildMembers | discordgo.IntentsGuildMessages)
+	b.c.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAllWithoutPrivileged | discordgo.IntentsGuildMembers | discordgo.IntentsGuildMessages | discordgo.IntentsDirectMessages)
 
 	err = b.c.Open()
 	if err != nil {
